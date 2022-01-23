145510201c4b3bf7d34ffc651739cd8aa7b8b800
TS93 <tschram93@yahoo.com>
2022-01-22 22:15:03 -0500

    search login and signup working

--- server/server.js
+++ server/server.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const { resolvers, typeDefs } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
-	resolvers,
 	typeDefs,
+	resolvers,
+	playground: true,
+	introspection: true,
 });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '../client/build/index.html'));
+	});
 }
-
-app.use(routes);
+// app.use(routes);
 
 db.once('open', async () => {
 	await server.start();
+
 	server.applyMiddleware({ app });
 
 	app.listen(PORT, () => {
-		console.log(`🌍 Now listening on localhost:${PORT}`);
+		console.log(`Now listening on localhost: ${PORT}`);
 		console.log(
-			`GraphQL server available at http://localhost:${PORT}${server.graphqlPath}`
+			`GraphQL playground avaialble at http://localhost:${PORT}${server.graphqlPath}`
 		);
 	});
 });
